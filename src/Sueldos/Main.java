cc7669cd4432b4e7b33e92408bcb47015fe3ded2
Conrado Chiesa <conradochiesa@gmail.com>
2019-09-15 14:36:29 -0300

    Primeros pasos en clases abstractas.

---
+++ src/Sueldos/Main.java
@@ -0,0 +1,13 @@
+package Sueldos;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //    Empleado e1 = new EmpleadoComision(10000,10000);
+    //    Empleado e2 = new EmpleadoHorasExtra(10000, 10);
+
+
+    //    System.out.println("Sueldo empleado 1: " + e1.getPago());
+    //    System.out.println("Sueldo empleado 2: " + e2.getPago());
+    }
+}
